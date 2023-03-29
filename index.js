e4c35786ccbece173ab0e2f667f629b7fc107eb5
GauravPandit-Webonise <gaurav.pandit@weboniselab.com>
2023-03-29 17:23:48 +0530

    feature js | javascript programs

---
+++ index.js
@@ -0,0 +1,7 @@
+function add(){
+document.getElementById("addanswer").value = Number(document.getElementById("num1").value) + Number(document.getElementById("num2").value);
+}
+
+function sub(){
+  document.getElementById("addanswer").value = Number(document.getElementById("num1").value) - Number(document.getElementById("num2").value);
+}
